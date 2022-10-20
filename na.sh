b1e60fd0e349ea759fc619f1f2fec38a998b700f
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-10-20 17:32:53 +0800

    Create na.sh

---
+++ na.sh
@@ -0,0 +1,33 @@
+mkdir /etc/naive
+chmod +x caddy
+    
+    mv /root/caddy /etc/naive/caddy    
+    
+    cat << EOF >/etc/naive/Caddyfile
+:443, 域名
+tls admin@seewo.com
+route {
+ forward_proxy {
+   basic_auth 用户名 密码
+   hide_ip
+   hide_via
+   probe_resistance
+  }
+ reverse_proxy  反代地址  {
+   header_up  Host  {upstream_hostport}
+   header_up  X-Forwarded-Host  {host}
+  }
+}
+EOF
+    cat <<EOF > /root/naive-cl.json
+{
+  "listen": "socks://127.0.0.1:1080",
+  "proxy": "https://用户名:密码@域名",
+  "log": ""
+}
+EOF
+    qvurl="naive+https://用户名:密码@域名:443?padding=false#Naive"
+    echo $qvurl > /root/naive-qvurl.txt
+    
+    cd /opt/naive
+    /opt/naive/caddy start
