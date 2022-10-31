46501a654c55d2471eb3a5617eed481694e08dc3
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-10-31 17:00:40 +0800

    Update naiveproxy.sh

--- naiveproxy.sh
+++ naiveproxy.sh
@@ -393,7 +393,8 @@ fi
 
 duoport(){
 naiveports=`cat /etc/caddy/Caddyfile 2>/dev/null | awk '{print $1}' | grep : | tr -d ',:'`
-green "\n当前naiveproxy代理正在使用的端口(请不要重复选择)：\n$naiveports"
+green "\n当前naiveproxy代理正在使用的端口(请不要重复选择)："
+blue "$naiveports"
 oldport1=`cat /etc/caddy/reCaddyfile 2>/dev/null | sed -n 1p | awk '{print $1}'| tr -d ',:'`
 insport
 sed -i "s/$oldport1/$port/g" /etc/caddy/reCaddyfile
